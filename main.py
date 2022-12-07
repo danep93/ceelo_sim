78e59a9b7a9a403224fcee3242fce876c0db6ee8
Daniel Epstein <depstein@ocrolus.com>
2022-12-06 22:52:32 -0500

    final version of code with all features

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from statistics import mean
-from constants import MAX_ROLLS, MAX_NUM_DICE, NUM_SIMULATIONS, SCORES
+from constants import draw_die, MAX_ROLLS, MAX_NUM_DICE, NUM_SIMULATIONS, SCORES
 import random
 from pdb import set_trace as bp
 
@@ -37,6 +37,7 @@ def compare_summaries(first,second):
 def summary_to_partial_roll(summary):
     return [summary[1]] * summary[0]
 
+
 def round_score_to_summary(score):
     summary = "(0,0)"
     rounded_score = round(score)
@@ -48,6 +49,7 @@ def round_score_to_summary(score):
     elif rounded_score > score:
         summary = "rounded up " + summary
     return summary
+
 
 def _simulate_all_rolls(ones_target, dice, starting_roll):
     # NOTE: runs num_simulations times
@@ -108,6 +110,7 @@ def simulate_remaining_rolls_for_probable_ones_targets(dice, curr_roll):
         ones_target_simulated_rolls[ones_target] = _simulate_all_rolls(ones_target, dice, curr_roll)
     return ones_target_simulated_rolls
 
+
 def get_round_simulated_summaries(ones_targets_to_simulated_rolls, ones_target, roll):
     round_simulated_rolls = {}
     # bp()
@@ -144,7 +147,6 @@ def print_win_ratios(fp_dice, curr_roll):
     fp_ones_targets_to_rolls = simulate_remaining_rolls_for_probable_ones_targets(fp_dice, curr_roll)
 
     # make cpu competitors and compare to current and future first player rolls
-    sims = 0
     for i in range(0, NUM_SIMULATIONS):
         curr_roll = og_curr_roll
         # CPU always starts from round 1, even if first player is comparing a 3rd round roll
@@ -164,12 +166,11 @@ def print_win_ratios(fp_dice, curr_roll):
                     win_lose_ratios[curr_roll][0] += 1  # win
                 elif result < 0:
                     win_lose_ratios[curr_roll][1] += 1  # loss
-                sims += 1
             win_lose_ones_targets[curr_roll] = fp_ones_target_future_roll
             cpu_avg_scores[curr_roll] = cpu_avg_scores[curr_roll] + cpu_round_summaries
             fp_avg_scores[curr_roll] = fp_avg_scores[curr_roll] + fp_round_summaries
             curr_roll += 1
-    # bp()
+
     print("Computer competitor (CPU) scores per round -- mostly static --")
     for k, v in cpu_avg_scores.items():
         cpu_avg_score = round(mean_score(cpu_avg_scores[k]), 2)
@@ -177,6 +178,7 @@ def print_win_ratios(fp_dice, curr_roll):
         print("Round: {} . . . CPU_avg_score: {} or {}".format(k, cpu_avg_score, cpu_rounded_score))
     print("\n")
 
+    print('\n'.join(map('  '.join, zip(*[draw_die(i) for i in fp_dice]))))
     print("dice are {} in round {}".format(fp_dice, og_curr_roll))
     print("First Player (FP) scores per round -- highly dependent on your initial dice --")
     for k, v in win_lose_ratios.items():
@@ -190,54 +192,15 @@ def print_win_ratios(fp_dice, curr_roll):
         fp_avg_score = round(mean_score(fp_avg_scores[k]), 2)
         fp_rounded_score = round_score_to_summary(fp_avg_score)
         ones_target = win_lose_ones_targets[k]
-        print(
-            "Round: {} . . . 1s_target: {} . . . FP_avg_score: {} or {} . . . Percentile: {} . . . Win-lose-ratio: {}".format(
-                k, ones_target, format(fp_avg_score, '.2f'), fp_rounded_score, format(percentile, '.0f'),
-                format(ratio, '.2f')))
+        print("Round: {} ... \t 1s_target: {} ... \t FP_avg_score: {} or {} ... \t Percentile: {} ... \t Win-lose-ratio: {}".format(
+            k, ones_target, format(fp_avg_score, '.2f'), fp_rounded_score, format(percentile, '.0f'),
+            format(ratio, '.2f')).expandtabs(25))
 
 
-# 1) prove why its better to go first
-# (5,5,1) is 88%
-# dice = [6,6,3,4,2]
-dice = [3,3,6,1,3]
-# print("dice are {} in round {} \n".format(fp_dice, curr_roll))
-curr_roll = 2
-final_roll = 3
+
+
+
+# When should you take the higher-concordance-lower-value dice over the lower-concordance-higher-value dice?
+dice = [4,4,1,5,6]
+curr_roll = 1
 print_win_ratios(dice, curr_roll)
-
-# print(simulate_remaining_rolls_for_probable_ones_targets(dice, curr_roll))
-
-# simulated_rolls = simulate_remaining_rolls_for_probable_ones_targets(dice, curr_roll)
-#
-# print("best ones target in round {} is {}".format(final_roll,
-#     get_best_ones_target_for_simulated_remaining_rolls(simulated_rolls, in_num_rolls=final_roll - curr_roll)))
-# print(simulated_rolls)
-
-
-# proves when you should use more of lesser number vs less of higher number for 1, 2, and 3 rounds
-# dice = [1,2,2,5,6]
-# simulated_rolls = simulate_remaining_rolls_for_probable_ones_targets(dice, current_roll=1, end_roll=3)
-# print(get_best_ones_target_for_simulated_remaining_rolls(simulated_rolls, 2))
-
-
-# prove assumption that 3 4s wins about half the time and that goes down in later rounds
-# talk about percentiles and win ratios and what that means relative to how many people you play with
-
-# two questions to answer. Why is it better to go first? When should I choose less of something vs more of something else
-# prove why it is an advantage to go first. You can quit when you have the best projected win percentage.
-# show how that is 1st round with 4 sixes. But if you need to beat (5,5,3) then you must keep rolling, decreasing your relative advantage
-
-# todo list
-#todo:(daniel.epstein) INCLUDE CURRENT SUMMARY IN LIST OF SUMMARIES!!! Just too complicated otherwise.
-#todo:(daniel.epstein) make spacing work for printing ratios so theyre always consistent
-#todo:(daniel.epstein) let them put in their own 1s target (or compare best to 2nd best so you can see HOW much better). In probable_best_ones_target allow override to pass in your own??
-#todo:(daniel.epstein) factor in ones where you hit 5 of something before 3rd round
-
-
-# the problem is if FP starts with round 2, array is smaller than CPU array. so you're comparing an array with 2 entries per entry with 1 that just has 1
-
-# todo list DONE
-#todo:(daniel.epstein) score should also say round up/down to <summary>
-#todo:(daniel.epstein) show that 5,5,3 has a lower win-lose ratio and is in a lower percentile than 4,6,1
-#todo:(daniel.epstein): fix next/current roll debacle so we can get win ratio of a roll in a certain round.
-#todo:(daniel.epstein) i suspect when you enter a roll in X round you're competing against cpu at 1st round. FIX THIS
